1a57c3319d6511090744cc4fef7d3a4f29040ad5
sdyolove <cs.yothin@gmail.com>
2017-07-17 16:45:47 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'Cc3m5HT+5TqG3j/7XIGfFZVuvTIglHxZTBo8Lt3EXnG3e6StHd57FCsViP6u4KZajIHfm0JwZtjXpWX4awxgacunoey6A8KUlSVI3jJgVOdNAePDSAEogfjLVs7hXVf+k8AlXA90in6/AJWhrJ5nIQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
